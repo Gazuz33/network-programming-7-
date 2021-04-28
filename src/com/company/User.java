4304b79ffc84225e824c5acd3541c2f1f54b16df
Andrew Goncharov <a4tonetak@gmail.com>
2021-04-29 00:57:28 +0300

    add user class

---
+++ src/com/company/User.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class User {
+
+    private String name;
+    private String addres;
+
+    public User (String name, String addres) {
+        this.name = name;
+        this.addres = addres;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+}
